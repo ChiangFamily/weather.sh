85636a11c6d4d9368c7c428fee76c5bf8cc78de9
Benjamin Perche <benjamin@perche.me>
2016-02-18 12:59:20 +0100

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,23 @@
+weather.sh
+===
+
+Small script that calls http://wttr.in to display a cli complient weather.
+
+Customize
+---
+
+You may edit the script to fallback on your prefered city (currently Lyon, France).
+
+Install
+---
+
+```sh
+# chmod +x weather.sh && mv weather.sh /usr/local/bin/weather
+```
+
+Usage
+---
+
+```sh
+$ weather [city]
+```
---
+++ weather.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+fallback="Lyon"
+command="curl -4 http://wttr.in/"
+
+curl --version > /dev/null 2> /dev/null
+
+if [ $? -ne 0 ]; then
+    echo "You need to install curl to run this script"
+    exit 2
+fi
+
+if [ $# -ge 2 ]; then
+    echo "Usage: $0 [city]"
+    exit 1
+fi
+
+if [ $# -eq 1 ]; then
+    city=$1
+else
+    city=$fallback
+fi
+
+$command$city
